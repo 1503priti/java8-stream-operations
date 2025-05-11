dc06557ea79881cc6afdfa55031aef748412ad80
Priti Kumari <151085priti@gmail.com>
2025-05-11 13:06:08 +0800

    initial commit

---
+++ src/main/java/com/pritiiitd/java8/streams/Java8StreamsApplication.java
@@ -0,0 +1,13 @@
+package com.pritiiitd.java8.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8StreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8StreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pritiiitd/java8/streams/Java8StreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritiiitd.java8.streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8StreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
